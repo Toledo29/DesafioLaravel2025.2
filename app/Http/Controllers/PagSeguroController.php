af4eeb61025efea73bc1c918427a41bad4f5aa5f
Toledo29 <gabrieltgb@gmail.com>
2025-09-24 16:29:23 -0300

    inicio integracao pagSeguro

---
+++ app/Http/Controllers/PagSeguroController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Produto;
+
+class PagSeguroController extends Controller
+{
+    public function comprar(Produto $produto){
+        
+    }
+}
